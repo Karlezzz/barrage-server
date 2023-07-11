941554584c379cd2fa33fe7b60f526a7bbeecf3c
karle <970587844@qq.com>
2023-07-11 18:11:13 +0800

    update mongoDB connect

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-require ('./lib/db/db')
+const db = require('./db/index')
+db(()=>{
+  console.log(1);
+})
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -23,6 +26,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
