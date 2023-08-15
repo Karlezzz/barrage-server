d46fb1ae8d249a0d58cc2a6f7df8dda5ed89165e
karle <970587844@qq.com>
2023-08-15 16:23:23 +0800

    update

--- routes/users.js
+++ routes/users.js
@@ -1,17 +1,17 @@
 var express = require('express');
 var router = express.Router();
-const { User } = require('../db/models/User')
+// const { User } = require('../db/models/User')
 
 router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
 
-router.post('/register', async (req, res, next) => {
-  const user = User.create({
-    username: req.body.username,
-    password: req.body.password
-  })
-  res.send(user)
-})
+// router.post('/register', async (req, res, next) => {
+//   const user = User.create({
+//     username: req.body.username,
+//     password: req.body.password
+//   })
+//   res.send(user)
+// })
 
 module.exports = router;
