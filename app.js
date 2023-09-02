7cf761eb95dea281b6744ecd23f32b84ee398dc7
karle <970587844@qq.com>
2023-09-02 18:13:17 +0800

    update uerLogin

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
 // app.use('/', indexRouter);
-// app.use('/users', usersRouter);
+app.use('/user', usersRouter);
 app.use('/socket', socketRouter)
 
 app.use(express.static('./dist'))
--- routes/users.js
+++ routes/users.js
@@ -1,17 +1,45 @@
 var express = require('express');
 var router = express.Router();
-// const { User } = require('../db/models/User')
+const { User } = require('../lib/models')
 
 router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
 
-// router.post('/register', async (req, res, next) => {
-//   const user = User.create({
-//     username: req.body.username,
-//     password: req.body.password
-//   })
-//   res.send(user)
-// })
+router.post('/userLogin', (req, res, next) => {
+  //get Room info and check password
+  //find user from sql
+  //set user or update user
+  //return token, user
+  const jwt = require('jsonwebtoken')
+  const secret = "barrageDGUTSecret111"
+
+
+  const { body } = req
+  const { roomId, name, id, originToken, ipAddress, password } = body
+  let token = ''
+
+  jwt.verify(originToken, secret, (err, decode) => {
+    if (err) {
+      token = jwt.sign({
+        name, id
+      }, secret, {
+        expiresIn: 60 * 60 * 2
+      });
+    } else {
+      token = originToken
+    }
+  })
+
+  const data = {
+    name,
+    roomId,
+    id,
+    token,
+    message: 'ok',
+    code: 200
+  }
+  res.send(data)
+})
 
 module.exports = router;
