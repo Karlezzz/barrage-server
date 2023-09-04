86b0ea13cd77b75a0232eec44c107ea98cc4be4a
karle <970587844@qq.com>
2023-09-04 23:27:46 +0800

    add school ip

--- routes/socket.js
+++ routes/socket.js
@@ -4,7 +4,8 @@ var router = express.Router()
 
 router.get('/', function (req, res, next) {
   const response = Response.init({
-    data: [{ socketUrl: 'ws://192.168.1.10:3001' }],
+    data: [{ socketUrl: 'ws://10.62.247.107:3001' }], //school
+    // data: [{ socketUrl: 'ws://192.168.1.10:3001' }], //home
   })
   res.send(response)
 })
