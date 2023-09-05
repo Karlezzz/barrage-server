8bb1e6345965d7becf19663264163f2edb78b849
karle <970587844@qq.com>
2023-09-06 00:01:27 +0800

    add send client url endpoint

--- app.js
+++ app.js
@@ -6,16 +6,19 @@ var logger = require('morgan')
 
 const cors = require('cors')
 
-const db = require('./db/index')
+// const db = require('./db/index')
+// const useSocket = require('./socket/index')
+// db(() => {
+// 	useSocket()
+// })
 const useSocket = require('./socket/index')
-db(() => {
-	useSocket()
-})
+useSocket()
 
 var indexRouter = require('./routes/index')
-var usersRouter = require('./routes/users')
-let socketRouter = require('./routes/socket')
+const usersRouter = require('./routes/users')
+const socketRouter = require('./routes/socket')
 const roomRouter = require('./routes/room')
+const clientRouter = require('./routes/client')
 const { default: mongoose } = require('mongoose')
 
 var app = express()
@@ -34,23 +37,24 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use('/user', usersRouter)
 app.use('/socket', socketRouter)
 app.use('/room', roomRouter)
+app.use('/client', clientRouter)
 
 app.use(express.static('./dist'))
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-	next(createError(404))
+  next(createError(404))
 })
 
 // error handler
 app.use(function (err, req, res, next) {
-	// set locals, only providing error in development
-	res.locals.message = err.message
-	res.locals.error = req.app.get('env') === 'development' ? err : {}
+  // set locals, only providing error in development
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
 
-	// render the error page
-	res.status(err.status || 500)
-	res.render('error')
+  // render the error page
+  res.status(err.status || 500)
+  res.render('error')
 })
 
 module.exports = app
