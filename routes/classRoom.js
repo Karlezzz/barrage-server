dc70dfda967731b2162b56919cc7a5051034cea6
karle <970587844@qq.com>
2023-09-09 13:06:00 +0800

    change isOnClass

--- routes/classRoom.js
+++ routes/classRoom.js
@@ -4,9 +4,13 @@ const router = express.Router()
 
 router.post('/', (req, res, next) => {
   const { body } = req
+  const _isOnClass = body.isOnClass
   console.log(body)
   const response = Response.init({
-    data: [body]
+    data: [{
+      ...body,
+      isOnClass:!_isOnClass
+    }]
   })
   //update sql
   res.send(response)
