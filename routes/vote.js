3c34fcf1f5baf3465d5f57240b87599f8d2aed10
karle <970587844@qq.com>
2023-09-15 17:02:38 +0800

    add dummy data and handle vote response

--- routes/vote.js
+++ routes/vote.js
@@ -12,14 +12,69 @@ router.post('/', (req, res, next) => {
   res.send(response)
 })
 
-router.put('/:id', (req,res,next) => {
+router.put('/:id', (req, res, next) => {
   const { body } = req
-  console.log(body)
-  const response = Response.init({
-    data: [body]
-  })
+  
   //update sql
+  const updateVoteData = updateVote(body,voteFromMongo)
+  const response = Response.init({
+    data: [updateVoteData]
+  })
   res.send(response)
 })
+
+function updateVote(_vote, vv) {
+  console.log(_vote);
+  const { vote, option, user } = _vote
+  const { voteOptions } = vv
+  const originOption = voteOptions.find(o => {
+    return option.id === o.id
+  })
+  console.log('oO',originOption);
+  if (originOption) {
+    const { selectMembers } = originOption
+    const hasUser = selectMembers.find(m => {
+      return m.id === user.id
+    })
+    console.log('user',hasUser);
+    if (hasUser) return voteFromMongo
+    selectMembers.push(user)
+  }
+  return vv
+}
+
+const voteFromMongo = {
+  question: 'How to learn Vue?',
+  id:'111',
+  content: 'test',
+  duration: 600000,
+  created: 1694765644129,
+  endTime: 1994765704129,
+  voteOptions: [
+    {
+      id: 'A2HRJMCVVvUZEMSJqzQQl',
+      optionValue: 'Online',
+      selectMembers: [
+        { name: 'Tom', id: '001' },
+      ],
+    },
+    {
+      id: 'A2HRJMCVVvUZEMSJqzQ23',
+      optionValue: 'Book',
+      selectMembers: [
+        { name: 'Karle', id: '011' },
+      ],
+    },
+    {
+      id: 'A2HRJMCVVvUDUHFSJqzQ23',
+      optionValue: 'Class',
+      selectMembers: [
+        { name: 'Joe', id: '005' },
+      ],
+    },
+  ],
+}
+
+
 
 module.exports = router
