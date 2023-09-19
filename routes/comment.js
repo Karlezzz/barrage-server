f0decc3266f069ab4ee7b6bb80eff8d1e89f0aa0
karle <970587844@qq.com>
2023-09-19 11:13:28 +0800

    add save comment to db

--- routes/comment.js
+++ routes/comment.js
@@ -2,13 +2,25 @@ const express = require('express')
 const { Response } = require('../lib/models')
 const router = express.Router()
 
-router.post('/', (req, res, next) => {
+const { CommentSchema } = require('../lib/models/Comment')
+const { MongoDB } = require('../db')
+const { instance } = CommentSchema.getInstance()
+const commentModel = instance
+let response
+
+router.post('/', async (req, res, next) => {
   const { body } = req
-  console.log(body)
-  const response = Response.init({
-    data: [body]
-  })
-  //update sql
+  try {
+    await MongoDB.connect()
+    const newComment = await commentModel.create(body)
+    response = Response.init({
+      data: [newComment]
+    })
+  } catch (error) {
+    console.log(error);
+  } finally {
+    MongoDB.disconnect()
+  }
   res.send(response)
 })
 
