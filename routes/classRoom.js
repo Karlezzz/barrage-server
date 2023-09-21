d6d0f7a3a4970f200cb3e176ed5feeab7c724a91
karle <970587844@qq.com>
2023-09-21 23:23:41 +0800

    update classRoom router

--- routes/classRoom.js
+++ routes/classRoom.js
@@ -2,17 +2,28 @@ const express = require('express')
 const { Response } = require('../lib/models')
 const router = express.Router()
 
-router.post('/', (req, res, next) => {
+const { MongoDB } = require('../db/index')
+const { ClassRoomSchema } = require('../lib/models/ClassRoom')
+const classRoomModel = ClassRoomSchema.getInstance().instance
+
+router.post('/', async (req, res, next) => {
   const { body } = req
-  const _isOnClass = body.isOnClass
-  console.log(body)
-  const response = Response.init({
-    data: [{
+  const _isOnClass = body.inOnClass
+  try {
+    await MongoDB.connect()
+    const classRoom = {
       ...body,
-      isOnClass:!_isOnClass
-    }]
-  })
-  //update sql
+      isOnClass: !_isOnClass
+    }
+    const newClassRoom = await classRoomModel.create(classRoom)
+    response = Response.init({
+      data: [newClassRoom]
+    })
+  } catch (error) {
+    console.log(error)
+  } finally {
+    MongoDB.disconnect()
+  }
   res.send(response)
 })
 
