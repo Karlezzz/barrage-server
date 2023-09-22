abd963a7e0d59057093500901fa7b3e83fcaf4a1
karle <970587844@qq.com>
2023-09-22 14:52:16 +0800

    save message to mongo

--- socket/index.js
+++ socket/index.js
@@ -6,6 +6,10 @@ const io = new Server(socketPort, { cors: true, transport: ['websocket'] })
 
 let so = null
 
+const { MongoDB } = require('../db/index')
+const { MessageSchema } = require('../lib/models/Message')
+const messageModel = MessageSchema.getInstance().instance
+
 httpProxy
   .createProxyServer({
     target: `http://localhost:${socketPort}`,
@@ -17,7 +21,20 @@ io.on('connection', socket => {
   so = socket
   console.log('user connected')
 
-  socket.on('sendMsg', data => {
+  socket.on('sendMsg', async data => {
+    const _data = JSON.parse(data)
+    const { type } = _data
+    if (type === 'chat') {
+      try {
+        await MongoDB.connect()
+        await messageModel.create(_data)
+
+      } catch (error) {
+        console.log(error)
+      } finally {
+        MongoDB.disconnect()
+      }
+    }
     io.sockets.emit('broadcast', data)
     console.log(`收到客户端的消息：${data}`)
   })
