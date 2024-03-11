41ac7cd8259fff750bd24a5b81d5b2874e718b0a
karle <970587844@qq.com>
2024-03-12 00:14:29 +0800

    feat: add get classRoomList from room id api

--- routes/classRoom.js
+++ routes/classRoom.js
@@ -10,44 +10,62 @@ const classRoomModel = ClassRoomSchema.getInstance().instance
 const roomModel = RoomSchema.getInstance().instance
 
 router.post('/', async (req, res, next) => {
-	try {
-		const { body } = req
-		const { id, inOnClass, ownerRoomCode } = body
-		const _isOnClass = !inOnClass
-		await MongoDB.connect()
-		const originClassRoom = await classRoomModel.findOne({ id })
-		const ownerRoom = await roomModel.findOne({ code: ownerRoomCode })
-		const { classRoomIds } = ownerRoom
-		if (!classRoomIds.find(i => i === id)) {
-			classRoomIds.push(id)
-			await roomModel.updateOne({ code: ownerRoomCode }, { classRoomIds })
-		}
-		if (originClassRoom) {
-			const updatedClassRoom = await classRoomModel.findOneAndUpdate(
-				{ id },
-				{ $set: { isOnClass: false } },
-				{ returnDocument: 'after' }
-			)
-			response = Response.init({
-				data: [updatedClassRoom],
-			})
-			io.sockets.emit('closeSocket')
-		} else {
-			const classRoom = {
-				...body,
-				isOnClass: _isOnClass,
-			}
-			const newClassRoom = await classRoomModel.create(classRoom)
-			response = Response.init({
-				data: [newClassRoom],
-			})
-		}
-	} catch (error) {
-		console.log(error)
-	} finally {
-		MongoDB.disconnect()
-	}
-	res.send(response)
+  try {
+    const { body } = req
+    const { id, inOnClass, ownerRoomCode } = body
+    const _isOnClass = !inOnClass
+    await MongoDB.connect()
+    const originClassRoom = await classRoomModel.findOne({ id })
+    const ownerRoom = await roomModel.findOne({ code: ownerRoomCode })
+    const { classRoomIds } = ownerRoom
+    if (!classRoomIds.find(i => i === id)) {
+      classRoomIds.push(id)
+      await roomModel.updateOne({ code: ownerRoomCode }, { classRoomIds })
+    }
+    if (originClassRoom) {
+      const updatedClassRoom = await classRoomModel.findOneAndUpdate(
+        { id },
+        { $set: { isOnClass: false } },
+        { returnDocument: 'after' }
+      )
+      response = Response.init({
+        data: [updatedClassRoom],
+      })
+      io.sockets.emit('closeSocket')
+    } else {
+      const classRoom = {
+        ...body,
+        isOnClass: _isOnClass,
+      }
+      const newClassRoom = await classRoomModel.create(classRoom)
+      response = Response.init({
+        data: [newClassRoom],
+      })
+    }
+  } catch (error) {
+    console.log(error)
+  } finally {
+    MongoDB.disconnect()
+  }
+  res.send(response)
+})
+
+router.get('/', async (req, res, next) => {
+  try {
+    const { query } = req
+    const { roomId } = query
+    await MongoDB.connect()
+    const ownerRoom = await roomModel.findOne({ id: roomId })
+    const { classRoomIds } = ownerRoom
+    response = Response.init({
+      data: [classRoomIds]
+    })
+  } catch (error) {
+    return Promise.reject(error)
+  } finally {
+    MongoDB.disconnect()
+  }
+  res.send(response)
 })
 
 module.exports = router
